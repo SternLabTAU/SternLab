2807c1b9be9d20a605e43d950e0bca493bc9d63a
taliak <taliakustin@gmail.com>
2017-06-15 11:59:22 +0300

    added a separator ability to merge function

--- pandas_utilities.py
+++ pandas_utilities.py
@@ -4,18 +4,19 @@ import pandas as pd
 from file_utilities import check_filename
 
 
-def merge_dfs(files, output):
+def merge_dfs(files, output, separator=","):
     """
     merges several dataframe files to output file
     :param files: input dataframe files paths
     :param output:  output file path
+    :param sep: separator for csv (default: ,)
     """
     files = [check_filename(f) for f in files]
     output = check_filename(output, Truefile=False)
-    dfs = [pd.read_csv(f) for f in files]
-    merged = df.append(dfs)
+    dfs = [pd.read_csv(f, sep=separator) for f in files]
     df = pd.DataFrame()
-    merged.to_csv(output)
+    merged = df.append(dfs)
+    merged.to_csv(output, index=False)
 
 
 
