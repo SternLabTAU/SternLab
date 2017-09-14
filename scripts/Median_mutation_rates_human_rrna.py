4fafed5dccbe4590527f9854becb7ebed671f579
okushnir <odedkushnir@gmail.com>
2017-09-14 12:40:07 +0300

    Support insertions/deletions

--- scripts/Median_mutation_rates_human_rrna.py
+++ scripts/Median_mutation_rates_human_rrna.py
@@ -18,26 +18,28 @@ def main():
     dir_path = "/Volumes/STERNADILABTEMP$/volume1/okushnir/Cirseq/CV/20170802_q30r2_merged_by_id/"
     plots_path = dir_path + "/plots/"
     freqs_path = dir_path + suffix
-    # mutation_file = freqs_path
-    # mutation_rates = freqs_to_dataframe(mutation_file)
     pathlib.Path(plots_path).mkdir(parents=True, exist_ok=True)
-    make_boxplot_mutation_median(freqs_path, plots_path, suffix)
+
+    make_boxplot_mutation_median(freqs_path, plots_path, suffix[:-6])
 
 
-def make_boxplot_mutation_median(freqs_file, out_dir, filename):
+def make_boxplot_mutation_median(freqs_file, out_dir, samplename):
     """
-
-    :param freqs_file:freqs file from the cirseq pipeline without gaps (N in command line)
-    :param out_dir: plots dir
-    :param filename: filename of the plots
+    :param freqs_file: freqs file from the cirseq pipeline
+    :param out_dir: The output directory
+    :param samplename: The objective name
     :return:
     """
     data = pd.read_table(freqs_file)
     data.reset_index(drop=True, inplace=True)
     flag = '-' in data.Base.values
     if flag is True:
-        raise Exception("This script does not support freqs file with deletions, for support please contact Maoz ;)")
-    data['Mutation Type'] = 'Non-Synonymous'
+        data = data[data.Ref != '-']
+        data = data[data.Base != '-']
+        data.reset_index(drop=True, inplace=True)
+        # data['abs_counts'] = data['Freq'] * data["Read_count"]  # .apply(lambda x: abs(math.log(x,10))/3.45)
+        # data['Frequency'] = data['abs_counts'] / data["Read_count"]
+        # raise Exception("This script does not support freqs file with deletions, for support please contact Maoz ;)")
     data['Base'].replace('T', 'U', inplace=True)
     data['Ref'].replace('T', 'U', inplace=True)
     min_read_count = 100000
@@ -46,20 +48,17 @@ def make_boxplot_mutation_median(freqs_file, out_dir, filename):
     data = data[data['Read_count'] > min_read_count]
     data['mutation_type'] = data['Ref'] + data['Base']
     data = data[data['Ref'] != data['Base']]
-    data = data[data["Base"] != "-"]
     data["Mutation"] = data["Ref"] + "->" + data["Base"]
     sns.set_palette(sns.color_palette("Paired", 12))
-    non_syn = data[data['Mutation Type'] == 'Non-Synonymous']
-    # non_syn = non_syn[non_syn['Pos'] > 1000]
-    # non_syn = non_syn[non_syn['Pos'] < 2800]
-    g1 = sns.boxplot(x="Mutation", y="Freq", data=non_syn,
-                     order=["A->C", "A->G", "A->U", "C->A", "C->G", "C->U", "G->A", "G->C", "G->U", "U->A",
-                            "U->C", "U->G"], color="DarkOrchid")
-    sns.set_style("darkgrid")
-    grouped_df = non_syn.groupby(["Mutation"])["Freq"]
-    for key, item in grouped_df:
-        print(grouped_df.get_group(key), "\n\n")
+    """ To Sample from the data"""
+    # data = data[data['Pos'] > 1000]
+    # data = data[data['Pos'] < 2800]
 
+    g1 = sns.boxplot(x="Mutation", y="Freq", data=data,
+                 order=["A->C", "A->G", "A->U", "C->A", "C->G", "C->U", "G->A", "G->C", "G->U", "U->A",
+                        "U->C", "U->G"], color="DarkOrchid")
+    grouped_df = data.groupby(["Mutation"])["Freq"]
+    len_col = grouped_df.size()
     medians = grouped_df.median()
     print(medians)
     median_labels = [str(np.round(s, 6)) for s in medians]
@@ -67,17 +66,16 @@ def make_boxplot_mutation_median(freqs_file, out_dir, filename):
     for tick, label in zip(pos, g1.get_xticklabels()):
         g1.text(pos[tick], medians[tick]*1.01, median_labels[tick],
                 horizontalalignment='center', size='x-small', color='w', weight='semibold', fontsize=5)
-
-    len_col = grouped_df.size()
     for tick, label in zip(pos, g1.get_xticklabels()):
         g1.text(pos[tick], medians[tick]+0.5, len_col[tick],
                 horizontalalignment='center', size='x-small', color='navy', weight='semibold', fontsize=5)
+    sns.set_style("darkgrid")
     g1.set(yscale="log")
     plt.legend(bbox_to_anchor=(1.0, 1), loc=2, borderaxespad=0., fontsize=7)
     g1.set_ylim(10 ** -6, 1)
     g1.tick_params(labelsize=7)
-    plt.title(filename + 'Mutations Frequencies', fontsize=19)
-    plt.savefig(out_dir + filename[:-6] + "_median_%s.png" % str(min_read_count), dpi=300)
+    plt.title(samplename + ' Mutations Frequencies', fontsize=19)
+    plt.savefig(out_dir + samplename + "_median_%s.png" % str(min_read_count), dpi=300)
     plt.close("all")
     print("The Plot is ready in the folder")
 
