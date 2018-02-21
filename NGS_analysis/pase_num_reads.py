a8876fe2aa4a267852dc3a240e61e229f0725571
Danielle <danimillers10@gmail.com>
2018-02-21 15:17:46 +0200

    reads coverage script

--- NGS_analysis/pase_num_reads.py
+++ NGS_analysis/pase_num_reads.py
@@ -11,13 +11,26 @@ def main(args):
     fastq_dir = args.fastq
     freq_dir = args.freq
 
-    fastq_files = [os.path.join(fastq_dir, f) for f in os.listdir(fastq_dir) if 'fastq' in f]
-    freq_files = [os.path.join(freq_dir, f) for f in os.listdir(freq_dir) if 'freq' in f]
+    fastq_files = []
+    freq_files = []
 
     sample_reads = []
     sample_coverage = []
     median_cov = []
     reads = []
+
+
+    for root, dirs, files in tqdm(os.walk(freq_dir)):
+        new_files = [os.path.join(root,f) for f in files if '.freq' in f and '_' not in f]
+        print(new_files)
+        freq_files.extend(new_files)
+
+    for root, dirs, files in tqdm(os.walk(fastq_dir)):
+        new_files = [os.path.join(root,f) for f in files if '.fastq' in f]
+        fastq_files.extend(new_files)
+
+    print(fastq_files)
+    print(freq_files)
 
     for f in tqdm(fastq_files):
         total_reads = get_num_reads(f)
@@ -25,16 +38,19 @@ def main(args):
         s = os.path.basename(f).split('.')[0]
         sample_reads.append(s)
 
-    for f in tqdm(freq_files):
+
+
+    for f in freq_files:
         coverage = get_median_coverage(f)
         median_cov.append(coverage)
-        s = os.path.basename(f).split('.')[0]
+        s = f.split('/')[-2]
         sample_coverage.append(s)
 
 
-    result = pd.DataFrame({'Median_coverage': median_cov, 'Sample_coverage':sample_coverage, 'Total_reads':reads,
-                           'Sample_reads':sample_reads})
-    result.to_csv(os.path.join(args.out, 'reads_and_coverage.csv'), index=False)
+    reads = pd.DataFrame({'Total_reads':reads,'Sample_reads':sample_reads})
+    coverage = pd.DataFrame({'Median_coverage': median_cov, 'Sample_coverage':sample_coverage})
+    reads.to_csv(os.path.join(args.out, 'reads.csv'), index=False)
+    coverage.to_csv(os.path.join(args.out, 'covergae.csv'), index=False)
 
 
 
@@ -60,6 +76,7 @@ def get_median_coverage(freq):
     """
 
     df = pd.read_csv(freq, sep='\t')
+    df = df.drop_duplicates(['Pos'])
     return np.median(df['Read_count'])
 
 
