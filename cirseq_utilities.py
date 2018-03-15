e8c0e08a9b2b7bb0bc30f5e51cd2b76627203b24
okushnir <odedkushnir@gmail.com>
2018-03-15 11:46:55 +0200

    Update 20180315 AA instead of Protein

--- cirseq_utilities.py
+++ cirseq_utilities.py
@@ -89,17 +89,14 @@ def find_mutation_type(freqs_file, ncbi_id):
     file_name = freqs_file
     data = freqs_to_dataframe(freqs_file)
     data.reset_index(drop=True, inplace=True)
-    # if ncbi_id != "NC_001490":
     start_pos, end_pos = find_coding_region(ncbi_id)
-    # else:
-    #     start_pos = 1
     data = data.loc[data['Pos'] >= start_pos]
     data = data.loc[data['Pos'] <= end_pos]
     if check_12mer(data) != 1:
-        raise Exception("The data is not in 12 mer. arrange the data, start_pos=%i, end_pos=%i" % start_pos, end_pos)
+        raise Exception("The data is not in 12 mer. arrange the data, start_pos=" + str(start_pos) + " end_pos=" + str(end_pos))
     data['Codon'] = ""
-    data['Ref_Protein'] = ""
-    data['Potential_Protein'] = ""
+    data['Ref_AA'] = ""
+    data['Potential_AA'] = ""
     data['Mutation Type'] = ""
     data['Pos'] = data['Pos'].astype(int)
     data.reset_index(drop=True, inplace=True)
@@ -111,7 +108,7 @@ def find_mutation_type(freqs_file, ncbi_id):
         print("translating codons....")
         translate_codon(kmer_df)
         print("Sets the Mutation type....")
-        kmer_df['Mutation Type'] = kmer_df[['Ref_Protein', 'Potential_Protein']].apply(
+        kmer_df['Mutation Type'] = kmer_df[['Ref_AA', 'Potential_AA']].apply(
             lambda protein: check_mutation_type(protein[0], protein[1]), axis=1)
     print("After a long for loop")
     file_name = file_name[0:-5]
@@ -146,14 +143,19 @@ def find_coding_region(ncbi_id):
         Entrez.email = "A.N.Other@example.com"
         handle = Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", id=ncbi_id)
         ncbi_gb = SeqIO.read(handle, "gb")
-
         handle.close()
-        if ncbi_id == "V01149":
-            location = list(ncbi_gb.features[5].location)
-        else:
-            location = list(ncbi_gb.features[1].location)
-        start_pos = location[0] + 1
-        end_pos = location[-1] + 1
+        start_pos = 0
+        end_pos = 0
+        # if ncbi_id == "V01149":
+        for feature in ncbi_gb.features:
+            if feature.type == 'CDS':
+                start_pos = feature.location.start + 1
+                end_pos = feature.location.end
+            #     location = list(ncbi_gb.features[5].location)
+            # else:
+            #     location = list(ncbi_gb.features[1].location)
+            # start_pos = location[0] + 1
+            # end_pos = location[-1] + 1
         return start_pos, end_pos
     except:
         print('Failed to fetch record.')
@@ -162,14 +164,14 @@ def find_coding_region(ncbi_id):
 def check_12mer(data):
     """
       :param data: pandas DataFrame of the freqs file
-      :return: None - checks that DataFrame is in 12 kmer
+      :return: 1/0; 1- the data in 12mer, 0 - ths data is NOT in 12mer checks that DataFrame is in 12 kmer
       """
     if (len(data)) % 12 != 0:
-        print('The data is not in 12 mer. arrange the data')
+        # print('The data is not in 12 mer. arrange the data')
         flag = 0
     else:
-        print('The data is in 12 mer. All OK.')
-        print('The length of data is:', (len(data)))
+        # print('The data is in 12 mer. All OK.')
+        # print('The length of data is:', (len(data)))
         flag = 1
     return flag
 
@@ -206,16 +208,16 @@ def translate_codon(data):
     :param data: pandas DataFrame of the freqs file
     :return: pandas DataFrame with Reference protein column and Potential protein column
     """
-    data['Ref_Protein'] = ""
-    data['Potential_Protein'] = ""
+    data['Ref_AA'] = ""
+    data['Potential_AA'] = ""
     protein_ref = Seq(data["Codon"][data.first_valid_index()]).translate()
-    data['Ref_Protein'].loc[data.first_valid_index():data.first_valid_index()+11] = protein_ref
-    data['Ref_Protein'].replace('(', '', inplace=True)
-    data['Ref_Protein'].replace(')', '', inplace=True)
+    data['Ref_AA'].loc[data.first_valid_index():data.first_valid_index()+11] = protein_ref
+    data['Ref_AA'].replace('(', '', inplace=True)
+    data['Ref_AA'].replace(')', '', inplace=True)
 
-    data["Potential_Protein"] = data["Codon"].apply(translate)
-    data["Potential_Protein"].replace('(', '', inplace=True)
-    data["Potential_Protein"].replace(')', '', inplace=True)
+    data["Potential_AA"] = data["Codon"].apply(translate)
+    data["Potential_AA"].replace('(', '', inplace=True)
+    data["Potential_AA"].replace(')', '', inplace=True)
     return data
 
 
