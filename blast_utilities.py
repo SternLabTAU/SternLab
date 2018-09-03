ee656e79caa0af9f3f84a5ebba42abc587f2d25a
noamharel161 <36629785+noamharel161@users.noreply.github.com>
2018-09-03 11:57:00 +0300

    fix in blast_utilities
    fixed event locations right after insertions

--- blast_utilities.py
+++ blast_utilities.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import re
+import math
 
 def blast_to_df(blast_path):
     '''
@@ -13,12 +14,13 @@ def blast_to_df(blast_path):
     blast_df.columns = ("read", "start_ref", "end_ref", "start_read", "end_read", "strand", "length", "btop")
     return blast_df
 
-def blast_to_mutations_list(blast_path, out_csv_path):
+def blast_to_mutations_list(blast_path, out_csv_path=None):
     '''
     Gets a blast file and creates a file containing all single nucleotide mutations
     (point mutations, insertions, and deletions) with the matching read name.
     :param blast_path: input file path, needs to be blast output
     :param out_csv_path: output file path
+    :returns: pandas dataframe
     '''
     blast = blast_to_df(blast_path)
     blast['parsed_btop'] = blast.apply(parse_btop, axis=1)
@@ -38,8 +40,9 @@ def blast_to_mutations_list(blast_path, out_csv_path):
     out['position'] = positions
     out['ref'] = refs
     out['base'] = bases
-    out.to_csv(out_csv_path, index=False)
-    return
+    if out_csv_path:
+        out.to_csv(out_csv_path, index=False)
+    return out
 
 def parse_btop(row):
     '''
@@ -59,14 +62,14 @@ def parse_btop(row):
     btops = pattern.findall(row['btop'])
     for b in btops:
         if number_pattern.findall(b) != []:
-            location = float(int(location) + int(b))
+            location = float(math.floor(location) + int(b))
         if mutation_pattern.findall(b) != []:
-            mutations.append((location, b))
-            location = float(int(location) + 1)
+            mutations.append((float(math.ceil(location)), b))
+            location = float(math.ceil(location) + 1)
         if insertion_pattern.findall(b) != []:
             location += 0.01
             insertions.append((location, b))
         if deletion_pattern.findall(b) != []:
-            deletions.append((location, b))
-            location = float(int(location) + 1) 
+            deletions.append((float(math.ceil(location)), b))
+            location = float(math.ceil(location) + 1) 
     return {'mutations':mutations, 'deletions':deletions, 'insertions':insertions}
