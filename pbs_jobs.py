fd7e2be21777c6210379e719a7bfc8f995ec86ee
Danielle <danimillers10@gmail.com>
2018-11-04 19:54:53 +0200

    changed python version to 3.6.1 in array runner

--- pbs_jobs.py
+++ pbs_jobs.py
@@ -58,7 +58,7 @@ def create_array_pbs_cmd(cmdfile, jnum, alias, gmem=7, cmds="", dir="", load_pyt
         o.write("date\n")
         o.write("hostname\n")
         if load_python:
-            o.write("module load python/anaconda_python-3.4.0\n")
+            o.write("module load python/anaconda_python-3.6.1\n")
 
         o.write("\n")
         o.write(cmds)
