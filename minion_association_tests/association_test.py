85c6044910ba0665317b3fdac5a4b907070ca2e6
noamharel161 <36629785+noamharel161@users.noreply.github.com>
2019-03-17 09:25:43 +0200

    Added scripts for minion association tests

---
+++ minion_association_tests/association_test.py
@@ -0,0 +1,61 @@
+#! /usr/local/python_anaconda/bin/python3.4
+
+
+import pandas as pd
+import numpy as np
+import os
+import scipy.stats
+import argparse
+
+
+def association_test(args):
+    blast_df = pd.read_csv(args.input_blast_df)
+    mutations_df = pd.read_csv(args.input_mutation_df)
+    association_test_dir = args.output_dir + '/' + str(args.pbs_job_array_id) + '/'
+    if not os.path.exists(association_test_dir):
+        os.mkdir(association_test_dir)    
+    couples = pd.read_csv(args.input_position_pairs_df)
+    position_tuple_list = list(couples[(couples.association_index == int(args.pbs_job_array_id))][['i','j']].itertuples(index=False, name=None))
+    
+    for (i, j) in position_tuple_list:
+        if not os.path.isfile(association_test_dir + str(i) + '_' + str(j)) and not os.path.isfile(association_test_dir + str(j) + '_' + str(i)):
+            print((i,j))
+            temp_matrix = pd.DataFrame(np.zeros(shape=(2,2)), columns=[j,0], index=[i,0])
+            b = blast_df.copy()
+            m = mutations_df.copy()
+            m = m[(m.position.isin([i,j]))]
+            b = b[(b.start_ref < i) & (b.end_ref > i) & (b.start_ref < j) & (b.end_ref > j)]
+            b = b[['read']]
+            relevant_mutations = pd.merge(b, m, on='read', how='left').fillna(0)
+            
+            reads_count = len(relevant_mutations[['read']].drop_duplicates())
+            reads_with_mut_i_mut_j  = len(pd.merge(relevant_mutations[(relevant_mutations.position == i)], relevant_mutations[(relevant_mutations.position == j)], on='read', how='inner').read.drop_duplicates())
+            reads_with_mut_j = len(relevant_mutations[(relevant_mutations.position == j)][['read']].drop_duplicates())
+            reads_with_mut_i = len(relevant_mutations[(relevant_mutations.position == i)][['read']].drop_duplicates())
+            reads_with_mut_i_wt_j = reads_with_mut_i - reads_with_mut_i_mut_j
+            reads_with_wt_i_mut_j = reads_with_mut_j - reads_with_mut_i_mut_j
+            reads_with_wt_i_wt_j = reads_count - reads_with_mut_i_mut_j - reads_with_mut_i_wt_j - reads_with_wt_i_mut_j
+            temp_matrix.at[i,j] = reads_with_mut_i_mut_j
+            temp_matrix.at[i,0] = reads_with_mut_i_wt_j
+            temp_matrix.at[0,j] = reads_with_wt_i_mut_j
+            temp_matrix.at[0,0] = reads_with_wt_i_wt_j
+            if temp_matrix.sum(axis=0).all() > 0 and temp_matrix.sum(axis=1).all() > 0:
+                a = (scipy.stats.chi2_contingency(temp_matrix, correction=False))
+                with open(association_test_dir + str(i) + '_' + str(j), 'w') as f:
+                    f.write('\n'.join([str(i) for i in a]))
+                temp_matrix.to_csv(association_test_dir + str(i) + '_' + str(j) + '.csv')
+    return
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--input_blast_df", type=str, help="path to blasts df csv", required=True)
+    parser.add_argument('-m', '--input_mutation_df', type=str, help='path to mutations df csv', required=True)
+    parser.add_argument('-i', '--input_position_pairs_df', type=str, help='path to position pairs index df, created by create_couples_index_file.py', required=True)
+    parser.add_argument("-o", "--output_dir", type=str, help="a path to an output directory", required=True)
+    parser.add_argument('-j', '--pbs_job_array_id', type=int, help='pbs job array id, used to choose pairs of positions to check.', required=True)
+    args = parser.parse_args()
+    if not vars(args):
+        parser.print_help()
+        parser.exit(1)
+    association_test(args)
---
+++ minion_association_tests/unify_association_results.py
@@ -0,0 +1,35 @@
+#! /usr/local/python_anaconda/bin/python3.4
+
+
+import pandas as pd
+import os
+import argparse
+
+def association_results_to_df(args):
+    data = []
+    results_directories = [args.input_results_directory + '/' + d for d in os.listdir(args.input_results_directory) if d.isnumeric()]
+    for file_dir in results_directories:
+        files = [file_dir + '/' + f for f in os.listdir(file_dir) if '.csv' not in f]
+        for f in files:
+            [i,j] = f.split('/')[-1].split('_')
+            i, j = int(i), int(j)
+            with open(f) as fi:
+                g = fi.read()
+                pvalue = float(g.split('\n')[1])
+                chi2 = float(g.split('\n')[0])
+                data.append((i,j,pvalue,chi2))
+                data.append((j,i,pvalue,chi2))
+    df = pd.DataFrame(data, columns=['pos1', 'pos2', 'pvalue', 'chi2'])
+    df.to_csv(args.output_csv, index=False)
+    return
+
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_results_directory", type=str, help="path to blasts df csv", required=True)
+    parser.add_argument("-o", "--output_csv", type=str, help="a path to an output directory to save results", required=True)
+    args = parser.parse_args()
+    if not vars(args):
+        parser.print_help()
+        parser.exit(1)
+    association_results_to_df(args)
