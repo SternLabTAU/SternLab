8d993fae448911ae7da4589c2e0671818da7b6f5
Talia Kustin <taliakustin@gmail.com>
2019-04-02 16:36:10 +0300

    analysis of mutation mappings

--- dirSel_utilities.py
+++ dirSel_utilities.py
@@ -6,7 +6,7 @@ from file_utilities import check_dirname, check_filename
 import pandas as pd
 import re
 from PAML_utilities import retrive_kappa_from_paml_output
-
+from phyVirus.get_baltimore import get_baltimore_classifiaction
 
 def write_params_file(param_file, input_aln, input_tree, output_res, output_log, output_tree, gtr_output=None):
 
@@ -61,3 +61,59 @@ def write_params_file(param_file, input_aln, input_tree, output_res, output_log,
 
     params.close()
     return params
+
+
+
+def return_branch_type(row):
+    """
+    check for each node if it's internal or external
+    :param row: dataframe row
+    :return: internal or external
+    """
+    #if the node name is N## than it's an internal node
+    if re.match("^N\d+", row["node_name"]) == None:
+        return "external"
+    return "internal"
+
+def analyze_dirSel_mutation_map(file, ratios_output=None):
+    file = check_filename(file)
+    if ratios_output == None:
+        ratios_output = file.split(".dirSel.results.mutation.map")[0] + ".dirSel.ratios"
+    else:
+        ratios_output = check_filename(ratios_output, Truefile=False)
+    base = file.split("/")[-1].split(".dirSel.results.mutation.map")[0]
+    family = base.split("_")[0]
+    baltimore = get_baltimore_classifiaction(family)
+    mapping = {0:"A", 1:"C", 2:"G", 3:"T"}
+    df = pd.read_csv(file, sep="\t", index_col=False)
+    if df.shape[0] == 0:
+        print("%s - has not mutation mapping" % file)
+        return
+    df["branch"] = df.apply(return_branch_type, axis=1)
+    df["count"] = 1
+    summary = df.groupby(["letter1", "letter2", "branch"], as_index=False).agg({"count": "count"})
+    for num,let in mapping.items():
+        summary.loc[summary.letter1 == num, "letter1"] = let
+        summary.loc[summary.letter2 == num, "letter2"] = let
+    summary["basename"] = base
+    summary["family"] = family
+    summary["baltimore"] = baltimore
+
+    ratios = pd.DataFrame()
+    for branch in ["external", "internal"]:
+        for nuc1 in mapping.values():
+            count_from_nuc1 = sum(summary.loc[(summary.letter1 == nuc1) & (summary.branch == branch), "count"].values)
+            for nuc2 in mapping.values():
+                if nuc1 == nuc2:
+                    continue
+                mut = nuc1 + nuc2
+                mut_count = list(summary.loc[(summary.letter1 == nuc1) &
+                                    (summary.letter2 == nuc2) & (summary.branch == branch), "count"].values)
+                if mut_count == []:
+                    mut_count.append(0)
+                ratio = float(mut_count[0]) /  count_from_nuc1
+
+
+                ratios = ratios.append({"basename":base, "family":family, "baltimore":baltimore, "branch":branch,
+                                        "substitution":mut, "ratio":ratio}, ignore_index=True)
+    ratios.to_csv(ratios_output)
